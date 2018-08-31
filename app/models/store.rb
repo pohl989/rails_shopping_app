4ba641ee71ca3b70dc70b4b77964fbcb892cdea7
pohl989 <pohl989@gmail.com>
2018-08-30 20:47:06 -0600

    uncomment my stuff

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,9 +5,9 @@ class Store < ApplicationRecord
   before_destroy :delete_associations
 
   # validations
-  # validates :name, presence: true
-  # validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
-  # validates_format_of :phone, with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/, on: :create
+  validates :name, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+  validates_format_of :phone, with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/, on: :create
 
   #Cool ENUM stuff
   enum state: { active: 0, inactive: 1}
